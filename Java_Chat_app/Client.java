3861cbba8bfcc2535cdff09559ed349a7e1713cb
Ramhari Munde <89252634+RamhariMunde@users.noreply.github.com>
2023-01-05 11:23:41 +0530

    Update Client.java

--- Java_Chat_app/Client.java
+++ Java_Chat_app/Client.java
@@ -1,3 +1,15 @@
+/* This is a java Application which will comunicate eith each other.
+To parform the comunication we use the consept of socket programing.
+
+import java.net.* :- ServerSocket Class is used for providing system-independent implementation of the server-side of a
+                     client/server Socket Connection.
+
+ Socket s :- it is a socket class object which is use to connect server socket.
+
+ while creating this object we have pass two paramiter as the IP address of Server & the Port Number of the Server Application.
+
+*/
+
 import java.net.*;
 import java.io.*;
 
@@ -8,12 +20,17 @@ public class Client
         System.out.println("Client application is running...");
 
         Socket s = new Socket("localhost",2100);
+        // it is a socket class object which is use to connect server socket.
         System.out.println("Client is waiting for the server to accept the request");
 
-        PrintStream ps = new PrintStream(s.getOutputStream());
+        PrintStream ps = new PrintStream(s.getOutputStream()); 
+        // this object is use the to send the data to the server Application as it is send the data it is considerd as output stream.
 
-        BufferedReader br1 = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br1 = new BufferedReader(new InputStreamReader(s.getInputStream())); 
+        // it is use the accept the input from  server input stream.
+
+        BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));        
+       // it is use to output from keyword
 
         String str1, str2;
 
