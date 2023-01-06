4dd44d13c6cc043958a7227c541e6d3c47d128ba
Ramhari Munde <89252634+RamhariMunde@users.noreply.github.com>
2023-01-07 00:03:26 +0530

    Update Server.java

--- Java_Chat_app/Server.java
+++ Java_Chat_app/Server.java
@@ -1,3 +1,9 @@
+/* This is a java Application which will comunicate eith each other.
+To parform the comunication we use the consept of socket programing.
+import java.net.* :- ServerSocket Class is used for providing system-independent implementation of the server-side of a
+                     client/server Socket Connection.
+*/
+
 import java.net.*;
 import java.io.*;
 
@@ -8,15 +14,19 @@ public class Server
         System.out.println("Server application is running...");
 
         ServerSocket ss = new ServerSocket(2100);
+        // it is a object of server socket class which is use to creat the server and start the server in lisning mode.
         System.out.println("Server is running at port no 2100 and waiting for client request");
 
         Socket s = ss.accept();
+        // it is object of socket class which is use to accept the request which cons from the client.
         System.out.println("Request of client gets accepted");
 
         PrintStream ps = new PrintStream(s.getOutputStream());
 
         BufferedReader br1 = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        // br1 it is object of print stream class use to send the data to the client application.
         BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
+        // br2 it is object of BufferedReader use to accept the Input from keyword.
         
         String str1, str2;
 
