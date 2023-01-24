afce617c541be371166ec78915c33ab64db7a8a9
Ramhari Munde <89252634+RamhariMunde@users.noreply.github.com>
2023-01-24 23:25:02 +0530

    Create Pattern1.java

---
+++ Pattern1.java
@@ -0,0 +1,46 @@
+class pattern1
+{
+    public static void main(String args[])
+    {
+        for(int i=1; i<=4; i++)
+        {
+            for(int j =1; j<=i; j++)
+            {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+}
+
+// pattern1 output 
+// *
+// **
+// ***
+// ****
+
+
+
+
+// class pattern2
+// {
+//     public static void main(String args[])
+//     {
+//         for(int i=1; i<=4; i++)
+//         {
+//             for(int j =1; j<=4; j++)
+//             {
+//                 System.out.print("*");
+
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// pattern2 output
+// ****
+// ****
+// ****
+// ****
