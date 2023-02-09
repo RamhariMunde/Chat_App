a7127f94b2d3b33d84b4b3b38dfc43f41fef746a
Ramhari Munde <89252634+RamhariMunde@users.noreply.github.com>
2023-02-09 23:25:06 +0530

    Update Client.java
    Client

--- Java_Chat_app/Client.java
+++ Java_Chat_app/Client.java
@@ -1,4 +1,4 @@
-/* This is a java Application which will comunicate eith each other.
+/* This a java Application which will comunicate eith each other.
 To parform the comunication we use the consept of socket programing.
 
 import java.net.* :- ServerSocket Class is used for providing system-independent implementation of the server-side of a
@@ -6,8 +6,8 @@ import java.net.* :- ServerSocket Class is used for providing system-independent
 
  Socket s :- it is a socket class object which is use to connect server socket.
  while creating this object we have pass two paramiter as the IP address of Server & the Port Number of the Server Application.
-
-*/
+ in this code us used  to only education parps 
+**/
 
 import java.net.*;
 import java.io.*;
@@ -16,7 +16,7 @@ public class Client
 {
     public static void main(String A[]) throws Exception
     {
-        System.out.println("Client application is running...");
+        System.out.println("Client application is running..");
 
         Socket s = new Socket("localhost",2100);
         // it is a socket class object which is use to connect server socket.
@@ -33,12 +33,12 @@ public class Client
 
         String str1, str2;
 
-        while(!(str1 = br2.readLine()).equals("end"))
+        while(!(str1 = br2.readLine()).equals("end"));
         {
             ps.println(str1);
             System.out.print("Enter message for server : ");
             str2 = br1.readLine();
             System.out.println("Server says : "+str2);
-        }
-    }
-}
+        };
+    };
+};
